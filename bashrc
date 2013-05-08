31218935e4bff2787ff881d2fbb19756bf406d53
Pavel Klimiankou <pashaklimenkov@gmail.com>
2013-05-07 21:59:33 -0400

    Improved install.sh. Added vim plugins folder and bashrc file

---
+++ bashrc
@@ -0,0 +1,5 @@
+export GREP_OPTIONS="--color"
+#export LSCOLORS="ExGxBxDxCxEgEdxbxgxcxd"
+alias ls="ls -G"
+
+PS1='\[\e[0;34m\][\u@\h\[\e[0m\] \[\e[0;32m\]\W\[\e[0m\]\[\e[0;34m\]]\[\e[0m\]\e[0;37m\]\$\[\e[0m\] '
--- install.sh
+++ install.sh
@@ -2,3 +2,30 @@
 ##################
 # Create symlinks from .files to ones in repository
 ##################
+
+target=~/dotfiles
+backup=~/dotfiles_old
+
+symlinks="vimrc bashrc vim"
+
+echo "Create backup folder"
+mkdir $backup
+echo "ok"
+
+cd $target
+
+for file in $symlinks; do
+	echo "backup .$file"
+	mv -v ~/.$file $backup/
+	echo "create link to $file"
+	ln -s $target/$file ~/.$file
+	echo "ok"
+done
+
+echo "Append bashrc to .profile"
+cat $target/bashrc >> ~/.profile
+echo "ok"
+
+cd -
+
+echo "Completed."
